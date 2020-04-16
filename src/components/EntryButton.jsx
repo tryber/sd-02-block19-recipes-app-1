9fe49bc4c0a23c5a9aac7b1f1a385084b7cbae23
Giorgia <giorgialeoniac@gmail.com>
2020-04-16 15:53:38 -0300

    Inserido o regex de e-mail sugerido pelo Johnatas

--- src/components/EntryButton.jsx
+++ src/components/EntryButton.jsx
@@ -3,7 +3,7 @@ import { RecipesContext } from '../context/Recipes';
 
 const EntryButton = () => {
   const { password, email } = useContext(RecipesContext);
-  const mailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  const mailRegex = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(-[a-z0-9]+)*(\.[a-z0-9]+(-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
   return (
     <div className="btn-div">
       <button
