fff1e1fbffae9ae1a45d452510304fcd01867e46
guicgs <etohlif@protonmail.com>
2020-04-16 18:42:00 -0300

    ...

--- src/pages/Receitas.js
+++ src/pages/Receitas.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
-<<<<<<< HEAD
-import SearchRecipes from '../components/SearchRecipes';
-=======
 import Footer from '../components/Footer';
->>>>>>> 02ccbc785fcef6974027a8e7c454fea581bb8092
 
 const Receitas = () => (
   <div>
     <Header />
-<<<<<<< HEAD
-    <SearchRecipes />
-=======
     <Footer />
->>>>>>> 02ccbc785fcef6974027a8e7c454fea581bb8092
   </div>
 );
 
