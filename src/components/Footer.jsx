4ad4b7dfdecbb77887a3725c5d323fb7d020881a
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-04-16 17:00:03 -0300

    CC #1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,17 +8,29 @@ import './Footer.css';
 const Footer = () => (
   <div className="footer-container">
     <Link to="/receitas/bebidas/">
-      <button className="footer-transparent-button" type="button">
+      <button
+        className="footer-transparent-button"
+        type="button"
+        data-testid="drinks-bottom-btn"
+      >
         <img className="footer-image" src={Cocktail} alt="Bebidas" />
       </button>
     </Link>
     <Link to="/receitas/explorar/">
-      <button className="footer-transparent-button" type="button">
+      <button
+        className="footer-transparent-button"
+        type="button"
+        data-testid="explore-bottom-btn"
+      >
         <img className="footer-image" src={Explorer} alt="Exploração" />
       </button>
     </Link>
     <Link to="/receitas/comidas/">
-      <button className="footer-transparent-button" type="button">
+      <button
+        className="footer-transparent-button"
+        type="button"
+        data-testid="food-bottom-btn"
+      >
         <img className="footer-image" src={ForkImg} alt="Comidas" />
       </button>
     </Link>
