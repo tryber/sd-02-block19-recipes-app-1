a01faa58c9b7237155dc2dac7659305db957b403
Giorgia <giorgialeoniac@gmail.com>
2020-04-22 20:41:37 -0300

    Iniciando branch de botão Iniciar Receita

---
+++ src/components/ReceitaButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ReceitaButton extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link>
+          <button
+            className="button-receita"
+          >
+            
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default ReceitaButton;
---
+++ src/context/ReceitaButtonContext.js
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react';
+
+const ReceitaButtonContext = createContext();
+
+Const RecButtonProvider = ({ children }) => {
+};
