1d4c58d6f98256fed536c895d44616711dbc8806
Giorgia <giorgialeoniac@gmail.com>
2020-04-24 19:49:15 -0300

    falta só tirar a pesquisa da página de perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { RecipesContext } from '../context/Recipes';
 import './Perfil.css';
 
-const Perfil = () => (
-  <div>
-    <Header />
-    <div>
-      <div data-testid="profile-email">
+const userEmail = JSON.parse(localStorage.getItem('user'));
 
+const Perfil = () => {
+  const { setHeaderTitle } = useContext(RecipesContext);
+  useEffect(() => {
+    setHeaderTitle('Perfil');
+  }, []);
+  return (
+    <div>
+      <Header />
+      <div>
+        <div className="user-mail" data-testid="profile-email">
+          {userEmail.email}
+        </div>
+        <div className="rectangles" data-testid="profile-done-btn">
+          <p>Receitas Feitas</p>
+        </div>
+        <div className="rectangles" data-testid="profile-favorite-btn">
+          <p>Receitas Favoritas</p>
+        </div>
+        <div className="rectangles" data-testid="profile-logout-btn">
+          <p>Sair</p>
+        </div>
       </div>
-      <div className="rectangles" data-testid="profile-done-btn">
-        <p>Receitas Feitas</p>
-      </div>
-      <div className="rectangles" data-testid="profile-favorite-btn">
-        <p>Receitas Favoritas</p>
-      </div>
-      <div className="rectangles" data-testid="profile-logout-btn">
-        <p>Sair</p>
-      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+}
 
 export default Perfil;
