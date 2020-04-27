fd76b7c202b730d4e72725f1a379f9da5220fc28
Giorgia <giorgialeoniac@gmail.com>
2020-04-27 16:35:24 -0300

    Correções CC

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -7,6 +7,34 @@ import './Perfil.css';
 
 const userEmail = JSON.parse(localStorage.getItem('user'));
 
+function renderPage() {
+  return (
+    <div>
+      <div className="user-mail" data-testid="profile-email">
+        {userEmail.email}
+      </div>
+      <Link to="/receitas-feitas">
+        <button className="rect" data-testid="profile-done-btn">
+          <p className="btn-perfil">Receitas Feitas</p>
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button className="rect" data-testid="profile-favorite-btn">
+          <p className="btn-perfil">Receitas Favoritas</p>
+        </button>
+      </Link>
+      <Link to="/">
+        <button className="rect"
+          data-testid="profile-logout-btn"
+          onClick={() => localStorage.clear()}
+        >
+          <p className="btn-perfil">Sair</p>
+        </button>
+      </Link>
+    </div>
+  );
+}
+
 const Perfil = () => {
   const { setHeaderTitle } = useContext(RecipesContext);
   useEffect(() => {
@@ -15,28 +43,7 @@ const Perfil = () => {
   return (
     <div>
       <Header showSearch={false} />
-      <div>
-        {userEmail.email}
-        <Link to="/receitas-feitas">
-          <button className="rect" data-testid="profile-done-btn">
-            <p className="btn-perfil">Receitas Feitas</p>
-          </button>
-        </Link>
-        <Link to="/receitas-favoritas">
-          <button className="rect" data-testid="profile-favorite-btn">
-            <p className="btn-perfil">Receitas Favoritas</p>
-          </button>
-        </Link>
-        <Link to="/">
-          <button
-            className="rect"
-            data-testid="profile-logout-btn"
-            onClick={() => localStorage.clear()}
-          >
-            <p className="btn-perfil">Sair</p>
-          </button>
-        </Link>
-      </div>
+      {renderPage()}
       <Footer />
     </div>
   );
