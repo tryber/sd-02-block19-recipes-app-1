d3e44e11bda745b15a7f96ca3ad9cf71663fb21d
Giorgia <giorgialeoniac@gmail.com>
2020-04-27 16:15:49 -0300

    CC

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -18,23 +18,21 @@ const Perfil = () => {
       <div>
         <div className="user-mail" data-testid="profile-email">
           {userEmail.email}
-        </div> 
+        </div>
         <Link to="/receitas-feitas">
-        <button className="rectangles" data-testid="profile-done-btn">
-          <p>Receitas Feitas</p>
-        </button>
+          <button className="rectangles" data-testid="profile-done-btn">
+            <p>Receitas Feitas</p>
+          </button>
         </Link>
         <Link to="/receitas-favoritas">
-        <button className="rectangles" data-testid="profile-favorite-btn">
-          <p>Receitas Favoritas</p>
-        </button>
+          <button className="rectangles" data-testid="profile-favorite-btn">
+            <p>Receitas Favoritas</p>
+          </button>
         </Link>
         <Link to="/">
-        <button className="rectangles" 
-        data-testid="profile-logout-btn"
-        onClick={() => localStorage.clear()}>
-          <p>Sair</p>
-        </button>
+          <button className="rectangles" data-testid="profile-logout-btn" onClick={() => localStorage.clear()}>
+            <p>Sair</p>
+          </button>
         </Link>
       </div>
       <Footer />
