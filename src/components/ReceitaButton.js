4278fff80bd1a1529d1f48a99ba4805efc193d40
Giorgia <giorgialeoniac@gmail.com>
2020-04-27 14:25:25 -0300

    Correção de CC

--- src/components/ReceitaButton.js
+++ src/components/ReceitaButton.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { RecipesContext } from '../context/Recipes';
 
 class ReceitaButton extends React.Component {
   render() {
     return (
+      const { buttonText } = useContext(RecipesContext);
       <div>
         <Link>
           <button
             className="button-receita"
             data-testid="start-recipe-btn"
           >
-            
+            {buttonText}
           </button>
         </Link>
       </div>
