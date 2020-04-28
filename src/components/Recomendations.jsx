7642774bfeb1290290682697b32995238065a1a0
guicgs <etohlif@protonmail.com>
2020-04-28 14:55:22 -0300

    carousel feito, mas ainda com css feio

--- src/components/Recomendations.jsx
+++ src/components/Recomendations.jsx
@@ -1,27 +1,27 @@
 import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
-// import Carousel from 'react-multi-carousel';
-// import 'react-multi-carousel/lib/styles.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import PropTypes from 'prop-types';
 import { RecipesContext } from '../context/Recipes';
 
-// const responsive = {
-//   desktop: {
-//     breakpoint: { max: 3000, min: 1024 },
-//     items: 3,
-//     slidesToSlide: 3, // optional, default to 1.
-//   },
-//   tablet: {
-//     breakpoint: { max: 1024, min: 464 },
-//     items: 2,
-//     slidesToSlide: 2, // optional, default to 1.
-//   },
-//   mobile: {
-//     breakpoint: { max: 464, min: 0 },
-//     items: 1,
-//     slidesToSlide: 1, // optional, default to 1.
-//   },
-// };
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 const Recomendations = ({ recipes }) => {
   const { setRecipeId } = useContext(RecipesContext);
@@ -51,30 +51,32 @@ const Recomendations = ({ recipes }) => {
   return (
     <section className="recomendations-section">
       <h2 className="details-titles">Recomendations</h2>
-      {recipes.length && recipes.map(({
-        idMeal,
-        idDrink,
-        strMeal,
-        strDrink,
-        strMealThumb,
-        strDrinkThumb,
-        strCategory,
-        strAlcoholic,
-      }) => (
-        <button
-          key={`${strMeal}-${Math.random() * 32}`}
-          style={{ background: 'none', border: 'none', cursor: 'pointer' }}
-          type="button"
-          className="MainContainerRecipe"
-          onClick={() => (idMeal
-            ? setRecipeId(idMeal) && <Redirect to={`/receitas/comidas/${idMeal}`} />
-            : setRecipeId(idDrink) && <Redirect to={`/receitas/bebidas/${idDrink}`} />)}
-        >
-          <img className="MainImg" src={strMealThumb || strDrinkThumb} alt={strMeal || strDrink} />
-          <p className="MainCategory">{strCategory || strAlcoholic}</p>
-          <p className="MainRecipe">{strMeal || strDrink}</p>
-        </button>
-      ))}
+      <Carousel responsive={responsive} infinite>
+        {recipes.length && recipes.map(({
+          idMeal,
+          idDrink,
+          strMeal,
+          strDrink,
+          strMealThumb,
+          strDrinkThumb,
+          strCategory,
+          strAlcoholic,
+        }) => (
+          <button
+            key={`${strMeal}-${Math.random() * 32}`}
+            style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+            type="button"
+            className="MainContainerRecipe"
+            onClick={() => (idMeal
+              ? setRecipeId(idMeal) && <Redirect to={`/receitas/comidas/${idMeal}`} />
+              : setRecipeId(idDrink) && <Redirect to={`/receitas/bebidas/${idDrink}`} />)}
+          >
+            <img className="MainImg" src={strMealThumb || strDrinkThumb} alt={strMeal || strDrink} />
+            <p className="MainCategory">{strCategory || strAlcoholic}</p>
+            <p className="MainRecipe">{strMeal || strDrink}</p>
+          </button>
+        ))}
+      </Carousel>
     </section>
   );
 };
--- src/hooks/useFetchRecomendations.jsx
+++ src/hooks/useFetchRecomendations.jsx
@@ -4,26 +4,28 @@ import { RecipesContext } from '../context/Recipes';
 
 const useFetchRecomendations = (type) => {
   const { recomendations, setRecomendations } = useContext(RecipesContext);
-  const [API, setAPI] = useState('themealdb');
+  const [API, setAPI] = useState('');
 
   useEffect(() => {
     if (type === 'comidas') setAPI('thecocktaildb');
     if (type === 'bebidas') setAPI('themealdb');
     const value = [];
-    const fetchRandom = async () => {
-      const url = `https://www.${API}.com/api/json/v1/1/random.php`;
-      const response = await axios.get(url);
-      const { meals = [], drinks = [] } = response.data;
-      const actual = (meals[0] || drinks[0]);
-      value.push(actual);
-      if (value.length === 6) {
-        setRecomendations(value);
+    if (API) {
+      const fetchRandom = async () => {
+        const url = `https://www.${API}.com/api/json/v1/1/random.php`;
+        const response = await axios.get(url);
+        const { meals = [], drinks = [] } = response.data;
+        const actual = (meals[0] || drinks[0]);
+        value.push(actual);
+        if (value.length === 6) {
+          setRecomendations(value);
+        }
+      };
+      for (let i = 0; i < 6; i += 1) {
+        fetchRandom();
       }
-    };
-    for (let i = 0; i < 6; i += 1) {
-      fetchRandom();
     }
-  }, [API, setAPI]);
+  }, [API]);
   return [recomendations];
 };
 
