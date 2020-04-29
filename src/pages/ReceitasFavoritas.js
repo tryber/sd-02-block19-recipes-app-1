e847c491762c54868f43aee26cbd60fe075f0b41
Giorgia <giorgialeoniac@gmail.com>
2020-04-29 19:31:21 -0300

    Favoritas

--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -2,16 +2,16 @@ import React, { useContext, useEffect } from 'react';
 import { RecipesContext } from '../context/Recipes';
 import Header from '../components/Header';
 
-
 const ReceitasFavoritas = () => {
   const { setHeaderTitle } = useContext(RecipesContext);
   useEffect(() => {
-    setHeaderTitle('Receitas Feitas');
+    setHeaderTitle('Receitas Favoritas');
   }, []);
+
   return (
-    <div>
+    <article>
       <Header showSearch={false} />
-    </div>
+    </article>
   );
 };
 
