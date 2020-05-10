5cc3348047395f669578ecb72bb781738d646273
Giorgia <giorgialeoniac@gmail.com>
2020-05-10 14:24:54 -0300

    Construindo os componentes

---
+++ src/components/favoritesList.jsx
@@ -0,0 +1,9 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { RecipesContext } from '../context/Recipes';
+
+
+const favoritesList = () => {}
+
+
+export default favoritesList;
--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,16 +1,20 @@
 import React, { useContext, useEffect } from 'react';
 import { RecipesContext } from '../context/Recipes';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const ReceitasFavoritas = () => {
   const { setHeaderTitle } = useContext(RecipesContext);
   useEffect(() => {
     setHeaderTitle('Receitas Favoritas');
   }, []);
+  const faveRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
 
   return (
     <article>
       <Header showSearch={false} />
+      <Footer />
     </article>
   );
 };
