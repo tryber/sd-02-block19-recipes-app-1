29b07be42351bfeca4b6f1f2d3f5763e62219fce
Giorgia <giorgialeoniac@gmail.com>
2020-05-11 13:21:36 -0300

    final

--- src/components/favoritesList.jsx
+++ src/components/favoritesList.jsx
@@ -1,16 +1,29 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import { RecipesContext } from '../context/Recipes';
 import FavoriteButton from './FavoriteButton';
 import ShareButton from './ShareButton';
 
+const updateAPI = (title) => {
+  if (title === 'bebidas') return 'thecocktaildb';
+  return 'themealdb';
+};
+
+const getDetailsPage = async (id, type, setFetchResult) => {
+  const url = `https://www.${updateAPI(type)}.com/api/json/v1/1/lookup.php?i=${id}`;
+  const response = await axios.get(url);
+  const value = response.data.meals || response.data.drinks;
+  setFetchResult(value);
+};
+
+
 const handleClick = (id, type, fetchResult, setFetchResult, history) => {
-  const goToRecipe = fetchResult.find((item) => item.idMeal === id || item.idDrink === id);
-  setFetchResult([goToRecipe]);
+  getDetailsPage(id, type, setFetchResult);
   history.push(`/receitas/${type}/${id}`);
 };
 
-const headerJSX = (
+const thumbJSX = (
   id, type, fetchResult, setFetchResult, strMealThumb, strDrinkThumb,
   strMeal, strDrink, history,
 ) => (
@@ -27,31 +40,46 @@ const headerJSX = (
   </button>
 );
 
-const mealsJSX = (idMeal, strArea, strCategory, strMeal, tags) => (
+const mealsJSX = (idMeal, strArea, strCategory, strMeal, strMealThumb) => (
   <div className="faveText">
     <div className="faveFlexySides">
       <span className="faveCategory">{`${strArea} - ${strCategory}`}</span>
     </div>
     <p className="faveRecipe">{strMeal}</p>
-    <p className="faveButtons">
-      {tags.map((tag) => <span key={tag} className="faveSearchBtn">{tag}</span>)}
-    </p>
     <div className="buttons-bottom">
-      <FavoriteButton url={`/receitas/comidas/${idMeal}`} />
-      <ShareButton />
+      <FavoriteButton
+        recipe={{
+          id: idMeal,
+          category: strCategory,
+          image: strMealThumb,
+          area: strArea,
+          name: strMeal,
+          isMeal: !!idMeal,
+        }}
+      />
+      <ShareButton url={`/receitas/comidas/${idMeal}`} />
     </div>
   </div>
 );
 
-const drinksJSX = (idDrink, strAlcoholic, strDrink) => (
+const drinksJSX = (idDrink, strAlcoholic, strDrink, strDrinkThumb, strArea) => (
   <div className="faveText">
     <div className="faveFlexySides">
       <span className="faveCategory">{`${strAlcoholic} Drink`}</span>
     </div>
     <p className="faveRecipe">{strDrink}</p>
     <div className="buttons-bottom">
-      <FavoriteButton url={`/receitas/bebidas/${idDrink}`} />
-      <ShareButton />
+      <FavoriteButton
+        recipe={{
+          id: idDrink,
+          category: strAlcoholic,
+          image: strDrinkThumb,
+          area: strArea,
+          name: strDrink,
+          isMeal: !idDrink,
+        }}
+      />
+      <ShareButton url={`/receitas/bebidas/${idDrink}`} />
     </div>
   </div>
 );
@@ -61,20 +89,17 @@ const FavoritesList = () => {
   const { fetchResult, setFetchResult } = useContext(RecipesContext);
   const history = useHistory();
   return (fetchResult.map(({
-    id, isMeal, category, image, area, name, strTags,
+    id, isMeal, category, image, area, name,
   }) => {
-    let tags = [];
     let type = 'comidas';
-    if (strTags) tags = strTags.split(',');
-    if (tags.length > 2) tags.length = 2;
-    if (id) type = 'bebidas';
+    if (!isMeal) type = 'bebidas';
     return (
       <div key={`${name}-${Math.random() * 32}`} className="faveContainerRecipe">
-        {headerJSX(id, type, fetchResult, setFetchResult,
+        {thumbJSX(id, type, fetchResult, setFetchResult,
           image, image, name, name, history)}
         {isMeal
-          ? mealsJSX(id, area, category, name, tags)
-          : drinksJSX(id, category, name)}
+          ? mealsJSX(id, area, category, name, image)
+          : drinksJSX(id, category, name, image)}
       </div>
     );
   }));
